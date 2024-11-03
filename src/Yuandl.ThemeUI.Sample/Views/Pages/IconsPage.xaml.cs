f8ad1a32b90baea0502af4ca062c1e909e8e289f
源动力 <127341626@qq.com>
2024-11-03 22:37:33 +0800

    修复Clipboard失败导致的bug

--- src/Yuandl.ThemeUI.Sample/Views/Pages/IconsPage.xaml.cs
+++ src/Yuandl.ThemeUI.Sample/Views/Pages/IconsPage.xaml.cs
@@ -1,7 +1,9 @@
 // This Source Code Form is subject to the terms of the MIT License.
 // If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
 // Copyright(C) Yuandl ThemeUI. All Rights Reserved.
+using Microsoft.Extensions.Logging;
 using Yuandl.ThemeUI.Sample.ViewModels.Pages;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Yuandl.ThemeUI.Sample.Views.Pages;
 
@@ -25,7 +27,19 @@ public partial class IconsPage : INavigableView<IconsPageViewModel>
     private void TextBlock_PreviewMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
     {
         System.Windows.Controls.TextBlock textBlock = (System.Windows.Controls.TextBlock)sender;
-        Clipboard.SetText(textBlock.Text);
-        _snackbarService.Show("友情提示", "复制成功", ControlAppearance.Primary);
+
+        try
+        {
+            Clipboard.SetText(textBlock.Text);
+            _snackbarService.Show("友情提示", "复制成功", ControlAppearance.Primary);
+        }
+        catch (Exception ex)
+        {
+            _snackbarService.Show("友情提示", "复制时发生错误："+ ex.Message, ControlAppearance.Primary);
+        }
+        finally
+        {
+            Clipboard.Flush();
+        }
     }
 }
